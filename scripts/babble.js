9de92cd262016542df0bf23e812b9d35edf869f3
Mikkel Kroman <mk@maero.dk>
2010-11-02 22:57:40 +0100

    Fixed some minor glitches and added some rendition just for show and tell.

--- scripts/babble.js
+++ scripts/babble.js
@@ -1,8 +1,13 @@
 var Babble = function(canvas) {
-  this.canvas = canvas;
+  this.width = canvas.attr('width');
+  this.height = canvas.attr('height');
+  this.context = canvas[0].getContext('2d');
   this.connection = new BMPConnection();
+
+  this.context.fillStyle = '#2C4D5C';
+  setInterval(this.draw(), 10);
 };
 
 Babble.prototype.draw = function() {
-  
+  this.context.fillRect(this.width * 0.5 - 100, this.height * 0.5 - 100, 200, 200);
 };
